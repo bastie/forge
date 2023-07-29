f2a6c0a17c269081aa797c17357a26102fb41a30
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2023-07-29 16:02:55 +0200

    github allows only Swift 5.7 today

--- Package.swift
+++ Package.swift
@@ -1,14 +1,20 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "forge",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "forge",
+            targets: ["forge"]),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
+        .target(
             name: "forge",
             path: "Sources"),
     ]
